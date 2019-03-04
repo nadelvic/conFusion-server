1286188f6db20338c63cc9f9b51fb85527021072
Nathan Delavictoire <nathan.delavictoire@gmail.com>
2019-03-04 17:26:04 +0100

    Passport facebook

--- authenticate.js
+++ authenticate.js
@@ -4,6 +4,7 @@ var User = require('./models/user');
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken');
+var FacebookTokenStrategy = require('passport-facebook-token');
 
 var config = require('./config');
 
@@ -50,16 +51,43 @@ exports.verifyUser = passport.authenticate('jwt', {session: false});
 // a token in the header.
 
 exports.verifyAdmin = (req,res,next) => {
-    User.findOne({_id: req.user._id})
-    .then((user) => {
-        if(user.admin){
-            next();
-        }
-        else {
-            var err = new Error('You are not authorized to perform this operation');
-            err.status = 403;
-            return next(err);
-        }
-    }, (err) => next(err))
-    .catch((err) => next(err))
-}
+    if(req.user.admin){
+        next();
+    }
+    else {
+        var err = new Error('You are not authorized to perform this operation');
+        err.status = 403;
+        return next(err);
+    }    
+};
+
+exports.facebookPassport = passport.use(
+    new FacebookTokenStrategy({
+        clientID: config.facebook.clientId,
+        clientSecret: config.facebook.clientSecret
+    },
+    (accessToken, refreshToken, profile, done ) => {
+        User.findOne({facebookId: profile.id}, (err,user) => {
+            if(err) {
+                return done(err,false);
+            }
+            if(!err && user !== null){
+                return done(null,user);
+            }
+            else {
+                // here we need to create a new user
+                user = new User({username: profile.displayName});
+                user.facebookId = profile.id;
+                user.firstname = profile.name.givenName;
+                user.lastname = profile.name.familyName;
+                user.save((err,user) => {
+                    if(err) return done(err,false);
+                    else {
+                        return done(null, user);
+                    }
+                });
+            }
+        })
+    }
+));
+
--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '1234-67890-09876-54321',
-    'mongoUrl':'mongodb://localhost:27017/conFusion'
+    'mongoUrl':'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '679222339163588',
+        clientSecret: '7deb72c4200445f779685cc3cb232a1e'
+    }
 }
